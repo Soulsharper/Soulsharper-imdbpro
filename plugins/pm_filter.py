0a89a9a5509be0ce87442814df41bc765f140255
Soul <86186525+Soulsharper@users.noreply.github.com>
2021-10-27 15:08:34 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -4,27 +4,6 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQ
 from pyrogram import Client, filters
 import re
 import random
-import imdb
-# create an instance of the IMDb class
-ia = IMDb()
-
-# get a movie
-movie = ia.get_movie('0133093')
-
-# print the names of the directors of the movie
-print('Directors:')
-for director in movie['directors']:
-    print(director['name'])
-
-# print the genres of the movie
-print('Genres:')
-for genre in movie['genres']:
-    print(genre)
-
-# search for a person name
-people = ia.search_person('Mel Gibson')
-for person in people:
-   print(person.personID, person['name'])
 
 from pyrogram.errors import UserNotParticipant
 from utils import get_filter_results, get_file_details, is_subscribed, get_poster
